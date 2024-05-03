95a8e6f4b5cafd26197fc79660ed632dba959085
leventekapitany <kap.levente@gmail.com>
2024-05-03 06:46:30 +0200

    cors

--- server/index.ts
+++ server/index.ts
@@ -10,7 +10,7 @@ import get from "./get";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors("*"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
